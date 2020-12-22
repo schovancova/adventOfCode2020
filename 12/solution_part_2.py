74d2ab803423ec09ce7563da39e3664cd6ed3df7
schovancova <sima.chovancova@gmail.com>
2020-12-22 19:30:34 +0100

    12 part 2

--- 12/solution_part_2.py
+++ 12/solution_part_2.py
@@ -1,25 +1,39 @@
+import math
+
+
+def rotate_waypoint(w_x, w_y, angle):
+    # rotate against (0,0)
+    x = w_x * round(math.cos(math.radians(angle))) + w_y * round(math.sin(math.radians(angle)))
+    y = - w_x * round(math.sin(math.radians(angle))) + w_y * round(math.cos(math.radians(angle)))
+    return x, y
+
+
 def main():
     with open('input.txt') as f:
         lines = f.read().splitlines()
+        # waypoint offset
+        w_x = 10
+        w_y = 1
+        # boat
         x = y = 0
-        rot = 90
         for line in lines:
             instr, num = line[:1], int(line[1:])
-            if instr == "N" or (instr == "F" and rot == 0):
-                y += num
-            elif instr == "S" or (instr == "F" and abs(rot) == 180):
-                y -= num
-            elif instr == "E" or (instr == "F" and rot in [90, -270]):
-                x += num
-            elif instr == "W" or (instr == "F" and rot in [270, -90]):
-                x -= num
+            if instr == "N":
+                w_y += num
+            elif instr == "S":
+                w_y -= num
+            elif instr == "E":
+                w_x += num
+            elif instr == "W":
+                w_x -= num
             elif instr == "R":
-                rot += num
-                rot -= 360 if rot >= 360 else 0
+                w_x, w_y = rotate_waypoint(w_x, w_y, num)
             elif instr == "L":
-                rot -= num
-                rot += 360 if rot <= -360 else 0
-
+                w_x, w_y = rotate_waypoint(w_x, w_y, -num)
+            elif instr == "F":
+                x += w_x * num
+                y += w_y * num
+            print(f"Waypoint x {w_x} {w_y}, Boat {x} {y}, Instruction {line}")
     print(f"Result is {abs(x)+abs(y)}")
 
 
